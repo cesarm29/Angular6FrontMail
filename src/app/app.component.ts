a223a1af559a76689ac623b40edad5570c28b953
cesar <cesar@cesars-MacBook-Pro.local>
2019-01-27 20:06:14 -0500

    commit one
    commit one

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { MessageService } from './services/message.service';
+import swal from 'sweetalert';
+
+@Component({
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ constructor(public _MessageService: MessageService) {
+}
+ contactForm(form) {
+ this._MessageService.sendMessage(form).subscribe(() => {
+ swal("Formulario de contacto", "Mensaje enviado correctamente", 'success');
+ });
+ }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { MessageService } from './services/message.service';
+@NgModule({
+ declarations: [
+ AppComponent
+ ],
+ imports: [
+ BrowserModule,
+ HttpClientModule,
+ FormsModule
+ ],
+ providers: [MessageService],
+ bootstrap: [AppComponent]
+})
+export class AppModule { }
